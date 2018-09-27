2d790e57405f6598692271cb991198aab5f4f52b
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2018-09-27 02:12:24 -0400

    convert roads/infrastructure signs to street signs versions (and un-register them and their recipes)

--- init.lua
+++ init.lua
@@ -1180,6 +1180,50 @@ minetest.register_lbm({
 	end
 })
 
+-- Convert old road/streets modpack signs to street_signs versions
+
+if minetest.get_modpath("infrastructure") then
+	local old_signs = {
+		"infrastructure:road_sign_stop",
+		"infrastructure:road_sign_stop_on_post",
+		"infrastructure:road_sign_yield",
+		"infrastructure:road_sign_yield_on_post",
+		"infrastructure:road_sign_crosswalk",
+		"infrastructure:road_sign_crosswalk_on_post"
+	}
+
+	local signs_equiv = {
+		["infrastructure:road_sign_stop"]              = "street_signs:sign_stop",
+		["infrastructure:road_sign_stop_on_post"]      = "street_signs:sign_stop_onpole",
+		["infrastructure:road_sign_yield"]             = "street_signs:sign_yield",
+		["infrastructure:road_sign_yield_on_post"]     = "street_signs:sign_yield_onpole",
+		["infrastructure:road_sign_crosswalk"]         = "street_signs:sign_pedestrian_crossing",
+		["infrastructure:road_sign_crosswalk_on_post"] = "street_signs:sign_pedestrian_crossing_onpole",
+	}
+
+	for _, name in ipairs(old_signs) do
+		minetest.unregister_item(name)
+		if not string.find(name, "on_post") then
+			minetest.clear_craft({output = name})
+		end
+	end
+
+	minetest.register_alias_force("infrastructure:road_sign_retroreflective_surface", "air")
+
+	minetest.register_lbm({
+		nodenames = old_signs,
+		name = "street_signs:convert_signs",
+		label = "Convert roads/streets modpack signs",
+		run_at_every_load = true,
+		action = function(pos, node)
+			local newname = signs_equiv[node.name]
+			local dir = minetest.facedir_to_dir(node.param2)
+			if not dir then return end
+			minetest.set_node(pos, {name = newname, param2 = minetest.dir_to_wallmounted(dir)})
+		end
+	})
+end
+
 if minetest.settings:get("log_mods") then
 	minetest.log("action", S("[MOD] Street signs loaded"))
 end
