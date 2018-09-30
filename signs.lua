7a317f1769f7a17a028c18e312e8f97bf75056e7
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2018-09-30 05:48:05 -0400

    Add left-pointing R10-3e

--- signs.lua
+++ signs.lua
@@ -1258,7 +1258,7 @@ for _, onpole in ipairs({"", "_onpole"}) do
 	})
 
 	minetest.register_node("street_signs:sign_ped_push_button_to_cross_r10_3e"..onpole, {
-		description = "R10-3e: Pedestrians, push button to cross",
+		description = "R10-3e: Pedestrians, push button to cross (pointing right)",
 		paramtype = "light",
 		sunlight_propagates = true,
 		paramtype2 = "wallmounted",
@@ -1277,6 +1277,28 @@ for _, onpole in ipairs({"", "_onpole"}) do
 		after_place_node = street_signs.after_place_node,
 		on_rotate = on_rotate,
 		drop = "street_signs:sign_ped_push_button_to_cross_r10_3e"
+	})
+
+	minetest.register_node("street_signs:sign_ped_push_button_to_cross_r10_3e_left"..onpole, {
+		description = "R10-3e: Pedestrians, push button to cross (pointing left)",
+		paramtype = "light",
+		sunlight_propagates = true,
+		paramtype2 = "wallmounted",
+		drawtype = "mesh",
+		node_box = cbox,
+		selection_box = cbox,
+		mesh = "street_signs_generic_sign_9x15"..onpole..".obj",
+		tiles = { "street_signs_ped_push_button_to_cross_r10_3e_left.png",
+			"street_signs_sign_edge.png",
+			pole_mount_tex
+		},
+		inventory_image = "street_signs_ped_push_button_to_cross_r10_3e_left_inv.png",
+		wield_image = "street_signs_ped_push_button_to_cross_r10_3e_left_inv.png",
+		groups = {choppy=2, dig_immediate=2, not_in_creative_inventory = nci},
+		default_color = "0",
+		after_place_node = street_signs.after_place_node,
+		on_rotate = on_rotate,
+		drop = "street_signs:sign_ped_push_button_to_cross_r10_3e_left"
 	})
 
 	minetest.register_node("street_signs:sign_ped_push_button_to_cross_r10_3i"..onpole, {
