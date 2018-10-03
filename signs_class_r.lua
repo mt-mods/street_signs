2aee97975406d784fdd5c29c378045c90f1139fb
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2018-10-03 00:16:17 -0400

    add R3-6 (and right/straight version)

--- signs_class_r.lua
+++ signs_class_r.lua
@@ -286,6 +286,51 @@ for _, onpole in ipairs({"", "_onpole"}) do
 		drop = "street_signs:sign_straight_through_only"
 	})
 
+	minetest.register_node("street_signs:sign_left_turn_or_straight"..onpole, {
+		description = "R3-6: Left turn or straight through",
+		paramtype = "light",
+		sunlight_propagates = true,
+		paramtype2 = "wallmounted",
+		drawtype = "mesh",
+		node_box = cbox,
+		selection_box = cbox,
+		mesh = "street_signs_generic_sign_30x36"..onpole..".obj",
+		tiles = {
+			"street_signs_left_turn_or_straight.png",
+			"street_signs_sign_edge.png",
+			pole_mount_tex
+		},
+		inventory_image = "street_signs_left_turn_or_straight_inv.png",
+		wield_image = "street_signs_left_turn_or_straight_inv.png",
+		groups = {choppy=2, dig_immediate=2, not_in_creative_inventory = nci},
+		after_place_node = street_signs.after_place_node,
+		on_rotate = on_rotate,
+		drop = "street_signs:sign_left_turn_or_straight"
+	})
+
+	minetest.register_node("street_signs:sign_right_turn_or_straight"..onpole, {
+		description = "R3-6: Right turn or straight through",
+		paramtype = "light",
+		sunlight_propagates = true,
+		paramtype2 = "wallmounted",
+		drawtype = "mesh",
+		node_box = cbox,
+		selection_box = cbox,
+		mesh = "street_signs_generic_sign_30x36"..onpole..".obj",
+		tiles = {
+			"street_signs_right_turn_or_straight.png",
+			"street_signs_sign_edge.png",
+			pole_mount_tex
+		},
+		inventory_image = "street_signs_right_turn_or_straight_inv.png",
+		wield_image = "street_signs_right_turn_or_straight_inv.png",
+		groups = {choppy=2, dig_immediate=2, not_in_creative_inventory = nci},
+		after_place_node = street_signs.after_place_node,
+		on_rotate = on_rotate,
+		drop = "street_signs:sign_right_turn_or_straight"
+	})
+
+
 	cbox = street_signs.make_selection_boxes(36, 36, onpole)
 
 	minetest.register_node("street_signs:sign_left_lane_must_turn_left"..onpole, {
